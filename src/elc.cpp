7756980c24284ffd12a81ecfbd194b5aefdfb451
Pavel <p.smokotnin@gmail.com>
2021-03-21 19:08:13 +0300

    fix ELC lock

--- src/elc.cpp
+++ src/elc.cpp
@@ -67,7 +67,7 @@ void ELC::setLoudness(float loudness)
 
 void ELC::update()
 {
-    std::lock_guard<std::mutex> guard(m_dataMutex);
+    m_dataMutex.lock();
 
     static const std::vector<double> fs {20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000,
                                          2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500};
@@ -103,6 +103,6 @@ void ELC::update()
         m_ftdata[i].magnitude = pow(10, (Lp - loudness()) / 20);
     }
 
-    guard.~lock_guard();
+    m_dataMutex.unlock();
     emit readyRead();
 }
